df8feed0142653474e9067a419602f9547bee483
Wesam Jabali <wesamjabali@icloud.com>
2021-03-18 00:26:43 -0500

    ChiBattery

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,8 @@
         <v-col cols="12">
           <v-card class="mx-auto d-flex" flat>
             <v-row align="center" justify="center">
-              <v-col :cols="$vuetify.breakpoint.smAndUp ? 2 : 12">
-                <div :align="$vuetify.breakpoint.smAndUp ? 'left' : 'center'">
+              <v-col :cols="$vuetify.breakpoint.mdAndUp ? 2 : 12">
+                <div :align="$vuetify.breakpoint.mdAndUp ? 'left' : 'center'">
                   <v-img
                     max-height="200"
                     max-width="200"
@@ -14,7 +14,7 @@
                   ></v-img>
                 </div>
               </v-col>
-              <v-col :cols="$vuetify.breakpoint.smAndUp ? 8 : 12">
+              <v-col :cols="$vuetify.breakpoint.mdAndUp ? 8 : 12">
                 <div class="title text-xl-h2 text-lg-h3 text-sm-h4 text-center">
                   <vue-typer
                     :text="[
@@ -39,7 +39,7 @@
             </v-row>
           </v-card>
         </v-col>
-        <v-col :cols="$vuetify.breakpoint.smAndUp ? 6 : 12">
+        <v-col :cols="$vuetify.breakpoint.mdAndUp ? 6 : 12">
           <div class="text-center title">My projects</div>
           <v-card
             v-for="project in projects"
@@ -50,11 +50,11 @@
           >
             <v-img :src="project.photo" height="200"> </v-img>
             <div class="text-center title py-5">{{ project.name }}</div>
-            <div v-html="project.description" class="text-center pb-5"></div>
+            <div v-html="project.description" class="text-center pb-5 mx-5"></div>
           </v-card>
         </v-col>
         <!-- Second col -->
-        <v-col :cols="$vuetify.breakpoint.smAndUp ? 6 : 12">
+        <v-col :cols="$vuetify.breakpoint.mdAndUp ? 6 : 12">
           <div class="text-center title">Here's some stuff I'm working on</div>
 
           <v-card
@@ -90,7 +90,8 @@ export default {
         {
           name: "BlueDaemon",
           url: "https://github.com/wesamjabali/BlueDaemon",
-          description: "A Discord bot to manage university servers.",
+          description:
+            "A Discord bot to manage university servers, written with <b>Node</b> and <b>DiscordJS</b>",
           photo:
             "https://github.com/wesamjabali/BlueDaemon/blob/main/assets/banner.png?raw=true",
         },
@@ -98,9 +99,17 @@ export default {
           name: "CDM Discussions",
           url: "https://discord.gg/4GbaSsvbDb",
           description:
-            "A Discord server I created and manage for DePaul University, consisting of about 500 members, including 7 faculty members.",
+            "A Discord server I created and manage for DePaul University, consisting of about <b>500 students</b> and <b>7 faculty members.</b>",
           photo:
             "https://github.com/wesamjabali/PersonalSite/blob/main/src/assets/server_banner_compact.png?raw=true",
+        },
+        {
+          name: "ChiBatterySystems Clock",
+          url: "https://chibatterysystems.com",
+          description:
+            "Created a time-sheet management system full-stack using <b>VueJS, NodeJS, ExpressJS</b> and <b>PostgreSQL</b> which is currently in use by over 20 employees on a daily basis.",
+          photo:
+            "https://i.ytimg.com/vi/y8HhPXo-Dxw/maxresdefault.jpg",
         },
         {
           name: "D3L Frontend",
