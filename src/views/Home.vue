fbc1ad7fc2fc7dab9a9e80de8ed88c8cd72764db
Wesam Jabali <wesamjabali@icloud.com>
2021-03-17 21:01:11 -0500

    typing animation

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,38 @@
 <template>
-<div>
-    <v-card class="mx-auto" flat> 
-        <div class="title text-center">Hello, I'm Wesam Jabali.</div>
-
-     </v-card>
-</div>
+  <div>
+    <v-card class="mx-auto" flat>
+      <div class="title text-left">
+        Hello, I'm Wesam Jabali and I'm
+        <vue-typer
+          :text="[
+            'a Software Developer',
+            'a DePaul University Graduate',
+            'a Community Leader',
+            'looking for work!',
+          ]"
+          :repeat="0"
+          :shuffle="false"
+          initial-action="typing"
+          :pre-type-delay="0"
+          :type-delay="70"
+          :pre-erase-delay="2000"
+          :erase-delay="50"
+          erase-style="backspace"
+          :erase-on-complete="false"
+          caret-animation="smooth"
+        ></vue-typer>
+      </div>
+    </v-card>
+  </div>
 </template>
 
 <script>
-export default {
-  name: 'Home',
-  props: {
-    msg: String
-  }
-}
-</script>
+import { VueTyper } from "vue-typer";
 
+export default {
+  name: "Home",
+  components: {
+    VueTyper,
+  },
+};
+</script>
